226f6aa05a4e94fbc598f0c2ca6d42f4a1257d8b
Roman Pigunov <jerome.malone@yandex.ru>
2026-02-14 17:19:27 +0300

    1

---
+++ scripts/photos.js
@@ -0,0 +1,25 @@
+const photography = {
+  "Ana De Armas - Blonde": 7,
+  Boat: 26,
+  Gongs: 100,
+  "No Time to Die": 11,
+  Partae: 27,
+  Photography: 205,
+}; // С сайта gregwilliams.com
+
+const hollywoodAuthentic = {
+  "Austin Butler": 19,
+  "Awards Journey - Emma Stone": 22,
+  Cannes: 16,
+  "Dakota Johnson": 21,
+  "David Corenswet": 14,
+  Gongs: 86,
+  "Kate Winslet": 10,
+  "Mikey Madison": 9,
+  "Paul Mescal": 19,
+  "Simon Pegg": 12,
+  "The Smashing Machine": 11,
+  Wolfs: 10,
+  "Zoe Saldana": 13,
+  Photography: 131,
+}; // С сайта hollywoodauthentic.com
